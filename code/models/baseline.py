65e0ff519647c0d110cadecfb918c2b28cbff902
avoyd <avoy.datta@stanford.edu>
2019-03-02 04:31:58 -0800

    baseline.py complete except for data

--- code/models/baseline.py
+++ code/models/baseline.py
@@ -20,6 +20,7 @@ import os
 
 from docopt import docopt
 from RCNN import Config, RCNN
+from torch.utils.data import DataLoader
 
 
 baseline_model_path = "../../trained_models/baseline/baseline.pt"
@@ -41,6 +42,7 @@ def train(args, config):
 	"""
 	Train baseline model
 	"""
+
 	print("Training initiated...")
 
 	baseline_step = 0.1
@@ -60,9 +62,22 @@ def train(args, config):
 
 	optimizer = torch.optim.SGD(model.parameters(), lr= baseline_step, momentum = baseline_momentum)
 
-	train_data_path = args['--load_path'] if args['--data_path'] else 
+	train_data_path = args['--data_path'] else None
+	if train_data_path == None: 
+		raise Exception("Training data path not fed in.")
 
-	##Read in data
+	print("Loading training data from {}".format(train_data_path))
+
+
+	##############LOAD TRAIN DATA and initiate train
+
+	data_train = ...
+	dataloader_train = DataLoader(data_train, batch_size = config.batch_sz)
+
+	print("Finished loading training data from {}".format(train_data_path))
+
+
+	##Read in data from train_data_path
 	## Parse news titles into averages of word embeddings over title
 
 	train_accs = []
@@ -81,31 +96,41 @@ def train(args, config):
 
 	model.train()
 
-	for epoch in range(init_epoch, num_epochs):
-		start = time.time()
+	try: 
+		for epoch in range(init_epoch, num_epochs):
+			start = time.time()
 
-		#INITIATE dataloader_train
-		with tqdm(total = num_batches * config.batch_sz) as pbar:
-			for index, sample in enumerate(dataloader_train):
+			#INITIATE dataloader_train
+			with tqdm(total = num_batches * config.batch_sz) as pbar:
+				for index, sample in enumerate(dataloader_train):
 
-				titles, tech_indicators, movement = sample['titles'], sample['tech_indicators'], sample['movement']
-				titles.to(device)
-				tech_indicators.to(device)
-				movement.to(device)
+					titles, tech_indicators, movement = sample['titles'], sample['tech_indicators'], sample['movement']
+					titles.to(device)
+					tech_indicators.to(device)
+					movement.to(device)
 
-				logits = model.forward(titles, tech_indicators)
-				loss = backprop(optimizer, logits, movement)
-				train_ctr += 1
-				accuracy = get_accuracy(logits, movement) #Accuracy over entire mini-batch
-				
-				train_losses.append(loss)
-				train_accs.append(accuracy)
-				#Training step
+					logits = model.forward(titles, tech_indicators)
+					loss = backprop(optimizer, logits, movement)
+					train_ctr += 1
+					accuracy = get_accuracy(logits, movement) #Accuracy over entire mini-batch
+					
+					train_losses.append(loss)
+					train_accs.append(accuracy)
 
-		if epoch % print_every == 0: 
-			print ("Epoch: {}, Training iter: {}, Time since start: {}, Loss: , Training Accuracy: {}".format(epoch, train_ctr, (time.time() - start), loss, accuracy))
+			if epoch % print_every == 0: 
+				print ("Epoch: {}, Training iter: {}, Time since start: {}, Loss: , Training Accuracy: {}".format(epoch, train_ctr, (time.time() - start), loss, accuracy))
 
-		if epoch % save_every == 0:
+			if epoch % save_every == 0:
+				print ("Saving model to {}".format(save_path))
+				torch.save({'epoch': epoch,
+							'model_state_dict': model.state_dict(), 
+							'optimizer_state_dict': optimizer.state_dict(), 
+							'loss': loss}, 
+							save_path)
+				print ("Saved successfully to {}".format(save_path))
+
+		except KeyboardInterrupt:
+			print("Training interupted...")
 			print ("Saving model to {}".format(save_path))
 			torch.save({'epoch': epoch,
 						'model_state_dict': model.state_dict(), 
@@ -125,6 +150,20 @@ def test(args, config):
 	#Initiate dataloader_test
 
 	#Load saved model
+
+	test_data_path = args['--data_path'] else None
+	if test_data_path == None: 
+		raise Exception("Test data path not fed in.")
+
+	print("Loading test data from {}".format(train_data_path))
+
+
+	##############LOAD TEST DATA and initiate dataloader as dataloader_test
+
+	data_test = ...
+	dataloader_test = DataLoader(data_test, batch_size = config.batch_sz)
+
+	print("Finished loading training data from {}".format(train_data_path))
 
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	load_path = baseline_model_path if !args['--load_path'] else args['--load_path']
@@ -183,8 +222,8 @@ def main():
 	if args['train']:
 
         train_losses, train_accs, loss, accuracy = train(args, config, baseline_model_path)
-        np.save(train_accs, 'train_accs.npy')
-        np.save(train_losses, 'train_losses.npy')
+        np.save(np.array(train_accs), 'train_accs.npy')
+        np.save(np.array(train_losses), 'train_losses.npy')
         print("Final training loss: {}, Final Training Accuracy: {}".format(loss, accuracy))
 
     else args['test']:
