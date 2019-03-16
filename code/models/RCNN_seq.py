1f9543da6961f9ec5781464847e2cc587de9cfc3
avoyd <avoy.datta@stanford.edu>
2019-03-16 15:18:25 -0700

    added back forward for rcnn seq

--- code/models/RCNN_seq.py
+++ code/models/RCNN_seq.py
@@ -93,6 +93,89 @@ class RCNN_seq(nn.Module):
 		titles: Batched Tensor of news embedding vectors. Shape: (Batch, window_len_days, num_titles_per_day, embed_size, max_words_in_title)
 		tech_indicators: Batched tensor of tech indicators. Each batch element has indicators (dim 7) for a window of size n = 5. Expected dims: (seq_len, batch, input_dim)
 	"""
+	def forward(self, titles, tech_indicators):
+		batch_sz = titles.size(0)
+
+		# print("Input titles shape: ", titles.shape)
+		# print("Input tech indicators: ", tech_indicators.shape)
+
+		titles_reshaped = titles.contiguous().view(batch_sz * titles.size(1) * titles.size(2), titles.size(3), titles.size(4)) 
+		#(batch * window_len_days * num_titles_day, num_filters_title, words_title - filter_sz_title + 1)
+		#print("reshape # 1:", titles_reshaped.shape)
+
+		conv_out_titles = self.conv_title(titles_reshaped) #Out: (batch, window_len_days, num_titles_day, num_filters_title, words_title - filter_sz_title + 1)
+		#print("conv_out_titles: ", conv_out_titles.shape)
+
+
+		conv_title_pool_out = self.max_pool_title(conv_out_titles) #Out: (batch * window_len_days * num_titles_day, num_filters_title, 1)
+		
+		conv_title_pool_out = conv_title_pool_out.contiguous().view(batch_sz, titles.size(1), titles.size(2), self.config.n_filters_title, 1) 
+		#(batch, window_len_days, num_titles_day, num_filters_title, 1)
+		#print("reshape back # 1:", conv_title_pool_out.shape)
+
+
+		conv_title_pool_out = conv_title_pool_out.squeeze(-1) #Out: (batch, window_len_days, num_titles_day, num_filters_title)
+
+		#print("Title pool: ",conv_title_pool_out.shape)
+
+		relud_pool_title = self.relu_title(conv_title_pool_out) #(batch, window_len_days, num_titles_day, num_filters_title)
+		# ##print(relud_pool.shape)
+		# dropped_relu = self.dropout(relud_pool) #(batch, num_filters, L - R + 2) #NOT SURE ABOUT DIMENSION DROPPED
+		# ##print(dropped_relu.shape)
+		relud_pool_title_reshaped = relud_pool_title.permute(0, 1, 3, 2) #(batch, window_len_days, num_filters_title, num_titles_day)
+
+		#print(relud_pool_title_reshaped.shape)
+		relud_pool_title_reshaped = relud_pool_title_reshaped.contiguous().view(batch_sz * self.config.window_len_titles, 
+																				self.config.n_filters_title, self.config.num_titles) #(batch * window_len_days, num_filters_title, num_titles_day)
+
+		#print("Conv day input :", relud_pool_title_reshaped.shape)
+
+		conv_out_day = self.conv_day(relud_pool_title_reshaped) #Out: (batch * window_len_days, num_filters_day, num_titles_day - kernel_sz_day + 1)
+		
+		#print("Conv_out_day: ", conv_out_day.shape)
+		
+		conv_day_pool_out = self.max_pool_title(conv_out_day) #Out: (batch * window_len_days, num_filters_day, 1)
+		#print("Conv day pool out: ", conv_day_pool_out.shape)
+		
+		conv_day_pool_out = conv_day_pool_out.contiguous().view(batch_sz, titles.size(1), self.config.n_filters_day, 1)
+		#(batch, window_len_days, num_filters_day, 1)
+		#print("reshape back # 2:", conv_day_pool_out.shape)
+
+		conv_day_pool_out = conv_day_pool_out.squeeze(-1) #Out: (batch, window_len_days, num_filters_day)
+
+		relud_pool_day = self.relu_day(conv_day_pool_out) #Out: (batch, window_len_days, num_filters_day)
+		#print("Relud_pool_day: ", relud_pool_day.shape)
+
+		# dropped_relu = self.dropout(relud_pool) #(batch, num_filters, L - R + 2) #NOT SURE ABOUT DIMENSION DROPPED
+		##print(dropped_relu.shape)
+		relud_pool_day_reshaped = relud_pool_day.permute(1, 0, 2) #Out: (window_len_days, batch, num_filters_day)
+
+		#print("tech_indicators shape: ", tech_indicators.shape, "relud_pool_day_reshaped shape: ", relud_pool_day_reshaped.shape)
+		concat_input = torch.cat((relud_pool_day_reshaped, tech_indicators), dim = 2) #Size: (window_len_days==seq_len_tech, batch, input_dim_tech + num_filters_day)
+
+		#print("LSTM inputs: ", concat_input.shape)
+
+		lstm_outputs, (last_hidden, last_cell) = self.lstm(concat_input) #outputs shape: (seq_len, batch, hidden_sz). Both hidden & cell are (2, batch, hidden_size)
+
+		lstm_outputs_reshaped = lstm_outputs.permute(1, 2, 0) #(batch, hidden_sz, seq_len)
+
+		# print(lstm_outputs_reshaped.shape)
+		last_hidden = last_hidden.view(batch_sz, -1) #out: (batch, 2 * hidden_size)
+		last_cell = last_cell.view(batch_sz, -1)
+
+		attn_proj = torch.matmul(lstm_outputs_reshaped, self.attn_vector)
+		attn_proj = attn_proj.squeeze(-1)
+		# print(self.attn_vector.data)
+		# print(self.attn_vector.data.grad)
+		# print("Attn proj: ", attn_proj.shape)
+
+		#print("Concatenated end states: ", end_states_concatenated.shape)
+
+		mapped_down = self.relu_day(self.map_down(concat_end_states))
+
+		output = self.log_softmax(self.map_to_out(mapped_down)) #(batch, 2)
+		#print(output.shape)
+		return output
 
 """
 RCNN_seq WITH ATTENTION
