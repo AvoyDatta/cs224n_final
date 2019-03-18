a0adecc142a203d424006c065c0d01ddf59b39f0
avoyd <avoy.datta@stanford.edu>
2019-03-18 10:49:15 -0700

    updated run

--- code/models/run.py
+++ code/models/run.py
@@ -37,7 +37,7 @@ print(sys.path)
 import data_utils
 
 from RCNN_base import Config_base, RCNN_base
-from RCNN_seq import Config_seq, RCNN_seq, RCNN_seq_attn
+from RCNN_seq import Config_seq, RCNN_seq, RCNN_seq_attn, RCNN_seq_2
 from RCNN_v2 import Config_v2, RCNN_v2
 from RCNN_v2_ti import Config_v2_ti, RCNN_v2_ti
 from RCNN import Config, RCNN
@@ -47,7 +47,7 @@ from torch.utils.data import DataLoader
 
 
 
-main_model_path = "../../trained_models/RCNN_seq/RCNN_seq.pt"
+main_model_path = "../../trained_models/RCNN_seq/RCNN_seq_2.pt"
 
 def backprop(optimizer, logits, labels):
 
@@ -81,7 +81,7 @@ def train(args, config):
 	#Stores hyperparams for model
 	
 
-	model = RCNN_seq(config)
+	model = RCNN_seq_2(config)
 	model.to(device)
 
 	optimizer = torch.optim.SGD(model.parameters(), lr= optimizer_step, momentum = optimizer_momentum)
@@ -250,7 +250,7 @@ def test(args, config):
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	load_path = main_model_path 
 
-	model =  RCNN_seq(config)
+	model =  RCNN_seq_2(config)
 	model.to(device)
 
 	if (load_path != None):  #If model is retrained from saved ckpt
