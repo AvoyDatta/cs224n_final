68cd0798ce1a9523fe52996fd8da10eabeefb4ff
Victoria Ding <victoriading19@gmail.com>
2019-03-17 18:17:33 -0700

    change variable names

--- code/models/svm.py
+++ code/models/svm.py
@@ -18,7 +18,7 @@ sys.path.append('../../data/glove.6B/')
 print(sys.path)
 
 import data_utils
-from RCNN_seq import Config_seq, RCNN_seq
+# from RCNN_seq import Config_seq, RCNN_seq
 
 from RCNN_base import Config_base, RCNN_base
 from RCNN import Config, RCNN
@@ -30,53 +30,38 @@ main_model_path = "../../trained_models/RCNN_seq/RCNN_seq.pt"
 
 
 batch_sz = 128
-config = Config_seq()
-model = RCNN_seq(config)
-load_path = main_model_path
-
-if (load_path != None):  # If model is retrained from saved ckpt
-
-
-	print("Loading model from {}".format(load_path))
-	if  torch.cuda.is_available():
-		checkpoint = torch.load(load_path)
-	else:
-		checkpoint = torch.load(load_path,map_location='cpu')
-	model.load_state_dict(checkpoint['model_state_dict'])
-	# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-	# epoch = checkpoint['epoch']
-	# loss = checkpoint['loss']
-	print("Model successfully loaded from {}".format(load_path))
+# config = Config_seq()
+# model = RCNN_seq(config)
+# load_path = main_model_path
+#
+# if (load_path != None):  # If model is retrained from saved ckpt
+#
+#
+# 	print("Loading model from {}".format(load_path))
+# 	if  torch.cuda.is_available():
+# 		checkpoint = torch.load(load_path)
+# 	else:
+# 		checkpoint = torch.load(load_path,map_location='cpu')
+# 	model.load_state_dict(checkpoint['model_state_dict'])
+# 	# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# 	# epoch = checkpoint['epoch']
+# 	# loss = checkpoint['loss']
+# 	print("Model successfully loaded from {}".format(load_path))
 
 # store
-data = data_utils.DJIA_Dataset('../../data/DJIA_table.csv', '../../data/Combined_News_DJIA.csv', start=1, end=510, randomize_sz=None)
 
-q1 = 400  # originally 1600
-q2 = 451  # 1800
-q3 = 502  # 1980
 
-data_train = utils.data.Subset(data, [i for i in range(q1)])
-
-data_test = utils.data.Subset(data,[i for i in range(q1+1,q2)])
-
-data_extended_test = utils.data.Subset(data, [i for i in range(q2+1, q3)])
-
-def run_svm():
+def run_svm(data_train,data_test,data_extended_test):
 	# train
 	train_input = []
 	train_labels = []
 	print("Loading training data for SVM...")
-	# with tqdm(total = len(dataloader_train)) as pbar: # 13
-	# 	for index, sample in enumerate(dataloader_train):
 	with tqdm(total = len(data_train))as pbar:
 		for i in range(len(data_train)):
 			titles, tech_indicators, movement = data_train[i]['titles'],data_train[i]['tech_indicators'],data_train[i]['movement']
 			titles = titles.unsqueeze(0)
 			tech_indicators = tech_indicators.unsqueeze(0).permute(1, 0, 2)
 			combined = np.concatenate((np.reshape(titles[:,4,:,:,:],-1),np.reshape(tech_indicators,-1)),axis=0)
-			# combined = concat_titles_tech(titles, tech_indicators).detach().numpy() #(5,128,71)
-			# combined = np.reshape(combined[4],newshape=-1)
-			# print(combined.shape)
 			train_input.append(combined)
 			train_labels.append(movement.numpy())
 			pbar.update(1)
@@ -86,7 +71,7 @@ def run_svm():
 
 	print("train input shape: ",train_input.shape )
 	print("Training started...")
-	svc = svm.SVC(kernel='rbf')
+	svc = svm.SVC(kernel='rbf',verbose=True)
 	svc.fit(train_input, train_labels)
 	print("Training finished... making predictions...")
 	train_pred = svc.predict(train_input)
@@ -95,13 +80,14 @@ def run_svm():
 	# test
 	test_input = []
 	test_labels = []
-	print("Loading test data...")
+	print("Loading validation (test) data...")
 	with tqdm(total = len(data_test))as pbar:
 		for i in range(len(data_test)):
 			titles, tech_indicators, movement = data_test[i]['titles'],data_test[i]['tech_indicators'],data_test[i]['movement']
 			titles = titles.unsqueeze(0)
 			tech_indicators = tech_indicators.unsqueeze(0).permute(1, 0, 2)
 			combined = np.concatenate((np.reshape(titles[:, 4, :, :, :], -1), np.reshape(tech_indicators, -1)), axis=0)
+			# combined = np.reshape(tech_indicators,-1)
 			# combined = concat_titles_tech(titles, tech_indicators).detach().numpy() #(5,128,71)
 			# combined = np.reshape(combined[4],newshape=-1)
 			test_input.append(combined)
@@ -121,15 +107,13 @@ def run_svm():
 	# extended test
 	extended_test_input = []
 	extended_test_labels = []
-	print("Loading extended_test data...")
+	print("Loading extended test data...")
 	with tqdm(total = len(data_extended_test))as pbar:
 		for i in range(len(data_extended_test)):
 			titles, tech_indicators, movement = data_extended_test[i]['titles'],data_extended_test[i]['tech_indicators'],data_extended_test[i]['movement']
 			titles = titles.unsqueeze(0)
 			tech_indicators = tech_indicators.unsqueeze(0).permute(1, 0, 2)
 			combined = np.concatenate((np.reshape(titles[:, 4, :, :, :], -1), np.reshape(tech_indicators, -1)), axis=0)
-			# combined = concat_titles_tech(titles, tech_indicators).detach().numpy() #(5,128,71)
-			# combined = np.reshape(combined[4],newshape=-1)
 			extended_test_input.append(combined)
 			extended_test_labels.append(movement.numpy())
 			pbar.update(1)
@@ -139,9 +123,59 @@ def run_svm():
 	extended_test_labels = np.squeeze(np.array(extended_test_labels))
 
 	extended_test_pred = svc.predict(extended_test_input)
-	print("extended_Test accuracy: ", np.mean(extended_test_pred == extended_test_labels))
+
+	train_accuracy = np.mean(train_pred == train_labels)
+	test_accuracy =  np.mean(test_pred == test_labels)
+	extended_test_accuracy =  np.mean(extended_test_pred == extended_test_labels)
+	print("Extended test accuracy : ", extended_test_accuracy)
 	print(classification_report(extended_test_labels, extended_test_pred))
 
+	return train_accuracy,test_accuracy,extended_test_accuracy
 
 
-run_svm()
+if __name__ == "__main__":
+	data = data_utils.DJIA_Dataset('../../data/DJIA_table.csv', '../../data/Combined_News_DJIA.csv', start=1, end=200, randomize_sz=None)
+
+	# q1 = 1600  # originally 1600
+	# q2 = 1800  # 1800
+	# q3 = 1980  # 1980
+	train_split = 0.8
+	test_split = 0.1
+	extended_test_split = 0.1
+
+	##chunk dataset
+	window_sz = 50
+	chunk_sz = len(data)/window_sz
+	chunk_end_idxs = np.linspace(0,len(data),chunk_sz,dtype=int)
+	print(chunk_end_idxs)
+	chunk_train_accs = []
+	chunk_test_accs = []
+	chunk_extended_test_accs = []
+	for i in range(1,len(chunk_end_idxs)): #dummy for loop
+		chunk = utils.data.Subset(data,[j for j in range(chunk_end_idxs[i-1],chunk_end_idxs[i])])
+		chunk_len = len(chunk)
+		# print("chunk len: ",chunk_len)
+		# print("chunk idxs: ",chunk_end_idxs[i-1],chunk_end_idxs[i])
+
+		max_idx_train = int(train_split*len(chunk))
+		max_idx_test = max_idx_train + int(test_split*len(chunk))
+		max_idx_extended_test = max_idx_test + int(test_split*len(chunk))
+		data_train = utils.data.Subset(chunk, [i for i in range(max_idx_train)])
+		# dataloader_train = DataLoader(data_train, batch_size = int(config.batch_sz))
+
+		data_test = utils.data.Subset(chunk,[i for i in range(max_idx_train,max_idx_test)])
+		# print("data_CHANGE!_test length: ",len(data_CHANGE!_test))
+		# dataloader_CHANGE!_test = DataLoader(data_CHANGE!_test,batch_size=int(batch_sz))
+
+		data_extended_test = utils.data.Subset(chunk, [i for i in range(max_idx_test, max_idx_extended_test)])
+		# print("data_test length: ",len(data_test))
+
+		# dataloader_test = DataLoader(data_test, batch_size = batch_sz)
+
+		train_acc, test_acc,extended_test_acc = run_svm(data_train,data_test,data_extended_test)
+		chunk_train_accs.append(train_acc)
+		chunk_test_accs.append(test_acc)
+		chunk_extended_test_accs.append(extended_test_acc)
+	print("avg training accuracy on windowed set: ",np.mean(chunk_train_accs))
+	print("avg test accuracy on windowed set: ",np.mean(chunk_test_accs))
+	print("avg extended_test accuracy on windowed set",np.mean(chunk_extended_test_accs))
